793262ae0ce8ea774931916348c5b0c87a27f155
Eike Wilke <eike.wilke@mailbox.org>
2022-10-02 16:04:08 +0200

    finished task 2.2

---
+++ kap2/task2.2_sale.py
@@ -0,0 +1,20 @@
+def calc_discount(disc, p):
+    return (disc / 100) * p
+
+
+price_message, money_message = "Wie viel Euro kostet der Artikel: ", "Wie viel Euro wurden bezahlt: "
+discount_message = "Wie viel Prozent Rabatt sollen gewährt werden: "
+price = float(input(price_message))
+discount = int(input(discount_message))
+discount_in_euro = calc_discount(discount, price)
+
+discounted_price = price - discount_in_euro
+discount_price_message = "Es müssen {:.2f}€ bezahlt werden.".format(discounted_price)
+print(discount_price_message)
+
+paid = float(input(money_message))
+
+result = paid - discounted_price
+
+formatted_float_value = "Es müssen {:.2f}€ zurückgegeben werden".format(result)
+print(formatted_float_value)
